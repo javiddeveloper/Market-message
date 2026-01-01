0b19f615d7a3632818147d0e86dfd7788f030c4f
javid <javiddeveloper@gmail,com>
2026-01-01 14:07:11 +0330

    feat: message screen

--- app/src/main/java/xyz/sattar/javid/marketmessage/ui/messages/MessagesScreen.kt
+++ app/src/main/java/xyz/sattar/javid/marketmessage/ui/messages/MessagesScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.sattar.javid.marketmessage.ui.theme.MarketMessageTheme
 
 @Composable
 fun MessagesScreen() {
@@ -15,5 +17,13 @@ fun MessagesScreen() {
         contentAlignment = Alignment.Center
     ) {
         Text(text = "مدیریت پیام‌ها", style = MaterialTheme.typography.headlineMedium)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MessagesScreenPreview() {
+    MarketMessageTheme {
+        MessagesScreen()
     }
 }
