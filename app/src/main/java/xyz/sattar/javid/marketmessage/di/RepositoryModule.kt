ba095b004e64c70693c2c64b257c839e49f78183
javid <javiddeveloper@gmail,com>
2026-01-01 16:11:20 +0330

    feat: Implement draft and device contact repository

--- app/src/main/java/xyz/sattar/javid/marketmessage/di/RepositoryModule.kt
+++ app/src/main/java/xyz/sattar/javid/marketmessage/di/RepositoryModule.kt
@@ -5,16 +5,33 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import xyz.sattar.javid.marketmessage.data.repository.CustomerRepositoryImpl
+import xyz.sattar.javid.marketmessage.data.repository.MessageDraftRepositoryImpl
 import xyz.sattar.javid.marketmessage.data.repository.MessageRepositoryImpl
 import xyz.sattar.javid.marketmessage.data.repository.ReadyMessageRepositoryImpl
 import xyz.sattar.javid.marketmessage.domain.repository.CustomerRepository
+import xyz.sattar.javid.marketmessage.domain.repository.MessageDraftRepository
 import xyz.sattar.javid.marketmessage.domain.repository.MessageRepository
 import xyz.sattar.javid.marketmessage.domain.repository.ReadyMessageRepository
 import javax.inject.Singleton
 
+import xyz.sattar.javid.marketmessage.data.repository.DeviceContactRepositoryImpl
+import xyz.sattar.javid.marketmessage.domain.repository.DeviceContactRepository
+
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindDeviceContactRepository(
+        deviceContactRepositoryImpl: DeviceContactRepositoryImpl
+    ): DeviceContactRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindMessageDraftRepository(
+        messageDraftRepositoryImpl: MessageDraftRepositoryImpl
+    ): MessageDraftRepository
 
     @Binds
     @Singleton
